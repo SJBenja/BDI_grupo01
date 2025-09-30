18b60450d0f8083437b7a4d6fbc90713cf5c0ba6
Juan Benjamin Sandoval <benja1908@hmail.com>
2025-09-30 16:07:13 -0300

    Incorporacion de Script DDL del proyecto

--- script/script_ddl_proyecto.sql
+++ script/script_ddl_proyecto.sql
@@ -1,6 +1,207 @@
--- SCRIPT TEMA "nombre del tema"
--- DEFINNICIÓN DEL MODELO DE DATOS
+---------------------------
+--BASE DE DATOS I
+--PROYECTO: INVENTARIO DE SOPORTE INFORMATICO - EQUIPO 1
+--INTEGRANTES:
+--          - Sandoval, Juan Benjamin
+--          - Sosa, Gustavo Daniel
+--          - Vallejos, Gustavo Oscar
+--          - Vargas, Hernán Ezequiel
+--DEFINICION MODELO DE DATOS.
+---------------------------
+use master
+/*PREGUNTAMOS SI EXISTE NUESTRA BASE DE DATOS PREVIAMENTE Y LA BORRAMOS PARA VOLVER A CREARLA
+IF EXISTS(SELECT NAME FROM SYS.DATABASES WHERE NAME = 'base_inventario;') 
+BEGIN
+DROP DATABASE base_inventario;
+END
+GO */
 
-CREATE DATATABLE nombre_base;
+CREATE DATABASE base_inventario;
+go;
+USE base_inventario;
+go;
 
-USE nombre_base;
+-------------------
+
+if object_id('departamento') is not null DROP TABLE departamento
+go
+---------------------
+if object_id('provincia') is not null DROP TABLE provincia
+go
+---------------------
+if object_id('distrito') is not null DROP TABLE distrito
+go
+---------------------
+if object_id('agente') is not null DROP TABLE agente
+go
+---------------------
+if object_id('division') is not null DROP TABLE division
+go
+---------------------
+if object_id('estado') is not null DROP TABLE estado
+go
+---------------------
+if object_id('hardware') is not null DROP TABLE hardware
+go
+---------------------
+if object_id('fabricante') is not null DROP TABLE fabricante
+go
+---------------------
+if object_id('estado') is not null DROP TABLE estado
+go
+---------------------
+if object_id('usuario') is not null DROP TABLE usuario
+go
+---------------------
+if object_id('perfil') is not null DROP TABLE perfil
+go
+---------------------
+if object_id('agente_inventario') is not null DROP TABLE agente_inventario
+go
+---------------------
+if object_id('inventario') is not null DROP TABLE inventario
+go
+---------------------
+
+--TABLA PROVINCIA --
+create table provincia
+     (cod_provincia int primary key, 
+      descripcion varchar (100) not null,
+      )
+go
+
+--TABLA DEPARTAMENTO--
+Create table departamento 
+	(cod_provincia int not null, 
+	cod_departamento int not null, 
+	descripcion varchar(100) not null,
+	Constraint PK_departamento PRIMARY KEY (cod_provincia, cod_departamento),
+	Constraint FK_departamento_pcia FOREIGN KEY (cod_provincia)  REFERENCES provincia(cod_provincia),
+	)					
+go
+
+--TABLA DISTRITO--
+Create table distrito 
+	(cod_provincia int not null, 
+	cod_departamento int not null, 
+	nro_distrito int not null ,
+	descripcion varchar (100) not null,
+	direccion varchar (100) not null,
+	telefono int not null,
+    Constraint PK_distrito PRIMARY KEY (cod_provincia, cod_departamento, nro_distrito),
+	Constraint FK_distrito_pcia FOREIGN KEY (cod_provincia,cod_departamento)  REFERENCES departamento(cod_provincia,cod_departamento),						 								 					     					     					     				     					     
+	)
+go
+
+--TABLA DIVISION--
+Create table division (cod_division int primary key, 
+					    descripcion varchar(100) not null,
+						tel_ip int not null,
+						)
+go
+
+--TABLA ESTADO--
+
+Create table estado (id_estado varchar (30) primary key, 
+					    descripcion varchar(100) not null,
+						)
+go
+
+--TABLA HARDWARE--
+Create table hardware (id_hard int primary key, 
+					    rubro varchar(30) not null,
+						descripcion varchar (100) not null,
+						)
+go
+
+--TABLA FABRICANTE--
+Create table fabricante (id_fabricante int primary key, 
+					    descripcion varchar(100) not null,
+						)
+go
+
+
+--TABLA AGENTE--
+
+create table agente
+      (cuil int not null,
+       nombre varchar (50) not null,
+	   apellido varchar (50) not null,
+	   direccion varchar (100) not null,
+	   telefono int not null,
+       cod_division int not null,
+       cod_provincia int not null,
+       cod_departamento int not null,
+	   nro_distrito int not null,
+	   Constraint PK_agente PRIMARY KEY (cuil),
+	   Constraint FK_agente_distrito FOREIGN KEY (cod_provincia, cod_departamento, nro_distrito)  REFERENCES distrito(cod_provincia, cod_departamento, nro_distrito),
+	   Constraint FK_agente_division FOREIGN KEY (cod_division) REFERENCES division(cod_division),
+	   )
+  go    
+  
+-- TABLA INVENTARIO-- 
+create table inventario
+      (id_inventario int IDENTITY PRIMARY KEY,
+       id_hard int not null,
+	   id_estado varchar (30) not null,
+	   nro_legajo int not null,
+       nro_serie int not null,
+       id_fabricante int not null,
+       modelo varchar (30) not null,
+	   observacion varchar (50) not null,
+	   Constraint FK_inventario_hardware FOREIGN KEY (id_hard) REFERENCES hardware(id_hard),
+	   Constraint FK_inventario_estado FOREIGN KEY (id_estado) REFERENCES estado (id_estado), 
+	   Constraint FK_inventario_fabricante FOREIGN KEY (id_fabricante) REFERENCES fabricante (id_fabricante),
+	   )	
+  go 
+
+  --TABLA PERFIL--
+Create table perfil 
+	( id_perfil int primary key, 
+	perfil varchar (30) not null,
+	)
+go
+
+--TABLA USUARIO--
+Create table usuario ( id_usuario int IDENTITY primary key, 
+					   id_perfil int not null,
+					   cuil int not null,
+					   contraseña varchar (32) not null,
+					   Constraint FK_usuario_perfil FOREIGN KEY (id_perfil) REFERENCES perfil (id_perfil),
+					   Constraint FK_usuario_agente FOREIGN KEY (cuil) REFERENCES agente (cuil),
+						)
+go
+--TABLA AGENTE_INVENTARIO--
+create table agente_inventario( id int primary key identity,
+								id_inventario int,
+								cuil int not null,
+								fecha_alta date not null,
+								fecha_baja date,
+								cod_provincia int not null,
+								cod_departamento int not null,
+								observaciones varchar(250),
+								CONSTRAINT FK_agenteInventario_inventario FOREIGN KEY (id_inventario)REFERENCES	inventario(id_inventario),
+								CONSTRAINT FK_agenteInventario_agente FOREIGN KEY (cuil) REFERENCES agente(cuil),
+								)
+GO
+
+--RESTRICCIONES DE TABLAS--
+/*1 - Un hardware no puede estar relacionado con dos agentes en la misma fecha, debe de tener una fecha de baja para la misma.
+ALTER TABLE agente_inventario
+	ADD CONSTRAINT CK_agenteInventario_idInventario CHECK (fecha_baja = not null);*/
+	
+/*4 - el número de legajo de hardware es único */
+ALTER TABLE inventario
+	ADD CONSTRAINT UQ_inventario_legajo UNIQUE (nro_legajo);
+
+/*3 - El registro de inventario es único*/
+ALTER TABLE inventario
+	ADD CONSTRAINT UQ_inventario_registro UNIQUE (id_inventario);
+
+/*2 - Cada Cuil de agente es único*/	
+ALTER TABLE agente
+	ADD CONSTRAINT UQ_agente_cuil UNIQUE (cuil);
+	
+/*6 - El estado de un hardware debe ser único, no se debe sobreponer  los estados a un hardware
+ALTER TABLE inventario
+	ADD CONSTRAINT UQ_inventario_cuil UNIQUE (cuil);*/
